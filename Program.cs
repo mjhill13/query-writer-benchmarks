08f378f7511c8c982c2aebcb697e06612adb5a1a
Matthew Hill <103188388+mjhill13@users.noreply.github.com>
2023-04-18 15:25:06 +0100

    feat: write cars to file

--- Program.cs
+++ Program.cs
@@ -1,12 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿using System.Globalization;
+using System.Reflection;
+using CsvHelper;
+using CsvHelper.Configuration;
+using Microsoft.EntityFrameworkCore;
 using WriteQueryTos3;
+
+const int pageSize = 10;
+string filePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "cars.csv");
 
 using (var context = new AppDbContext())
 {
     var query = context.Cars
+        .AsNoTracking()
         .OrderBy(x => x.Make)
         .ThenBy(x => x.Model);
-    
-    
+
+    var totalCount = await query.CountAsync();
+
+    var numPages = (totalCount + pageSize) / pageSize;
+
+    for (var i = 0; i < numPages; i++)
+    {
+        var cars = query.Skip(i).Take(pageSize);
+
+        await using var textWriter = new StreamWriter(filePath, true);
+        var csvWriter = new CsvWriter(textWriter, new CsvConfiguration(CultureInfo.InvariantCulture));
+        await csvWriter.WriteRecordsAsync(cars);
+    }
 }
