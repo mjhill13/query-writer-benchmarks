f68b5fea55f89b43e51ec86b17fc379408698d70
Matthew Hill <103188388+mjhill13@users.noreply.github.com>
2023-04-19 12:14:07 +0100

    refactor: add global consts

--- Consts.cs
+++ Consts.cs
@@ -2,7 +2,7 @@ namespace WriteQueryTos3;
 
 public static class Consts
 {
-    public const int MinCars = 500;
-    public const int MaxCars = 1000;
-    public const int BatchSize = 10;
+    public const int MinCars = 1000000;
+    public const int MaxCars = 1000000;
+    public const int BatchSize = 100;
 }
